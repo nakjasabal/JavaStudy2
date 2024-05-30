d42f535f653c4ff0eb779885e9c370ef484fe821
nakjasabal <nakjasabal@naver.com>
2024-05-30 17:57:12 +0900

    24년5월30일 Java강의

---
+++ src/ex07string/Test.java
@@ -0,0 +1,19 @@
+package ex07string;
+
+import java.util.Scanner;
+ 
+public class Test {
+
+	public static void main(String[] args) {
+		
+		"한글의 길이는 어떻게?".length();
+		
+		String str = "한글의 길이는 어떻게?";
+		str.length();
+		
+		Scanner scanner = new Scanner(System.in);
+		int inputNumber = scanner.nextInt();
+
+	}
+
+}
