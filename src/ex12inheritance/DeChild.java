aa69c76bf62230d65fcdaf668d9368a3bd85e70a
nakjasabal <nakjasabal@naver.com>
2024-06-07 17:49:46 +0900

    24년6월7일 Java수업

--- src/ex12inheritance/DeChild.java
+++ src/ex12inheritance/DeChild.java
@@ -1,34 +1,37 @@
 package ex12inheritance;
 
-//자식클래스 : DeParent클래스의 기능을 상속하여 확장한다. 
+//자식클래스 
 public class DeChild extends DeParent {
-	//멤버변수
+	//멤버변수 
 	String stNumber;
 	
 	//생성자 
 	public DeChild(String name, int age, String stNumber) {
-		/* 자식은 부모의 생성자 호출을 통해 먼저 부모인스턴스를 
-		생성한 후 자신의 인스턴스를 생성해야한다. */
+		/*
+		자식은 부모의 생성자 호출을 통해 먼저 부모인스턴스를 생성한 후
+		자신의 인스턴스를 생성해야한다. 
+		 */
 		super(name, age);
 		this.stNumber = stNumber;
 	}
 	
-	/* 자식클래스에서 확장한 메서드로 부모클래스에는 정의되어 
-	있지않다. */
+	/*
+	자식클래스에서 확장한 메서드로 부모클래스에는 정의되어 있지않다. 
+	 */
 	public void study() {
-		/* 부모의 멤버변수 name은 private이므로 자식에서 
-		사용하기 위해서는 getter를 통해 접근해야한다. */
+		/*
+		부모의 멤버변수 name은 private이므로 자식에서는 getter를 
+		통해 값을 반환받아야 한다. 
+		 */
 		System.out.println(getName() +"이(가) 공부한다.");
-		
 		//아래와 같이 직접 접근하면 에러가 발생한다. 
-		//System.out.println(name +"이(가) 공부한다.");
+//		System.out.println(name +"이(가) 공부한다.");
 	}
 	
 	/*
-	오버라이딩 아님 : 부모클래스에서 private 멤버로 선언하
-	 	였으므로 자식쪽에서는 보이지 않아 오버라이딩의 대상이
-	 	될수없다. 해당 메서드는 자식쪽에서 새롭게 확장한 
-	 	메서드가 된다.  
+	오버라이딩 아님 : 부모클래스에서 private 멤버로 선언했으므로
+		자식쪽에서는 보이지 않아 오버라이딩의 대상이 될수없다. 
+		해당 메서드는 자식쪽에서 새롭게 확장한 메서드가 된다. 
 	*/
 	private void eat() {
 		System.out.println("학생이 먹는다.");
@@ -36,10 +39,10 @@ public class DeChild extends DeParent {
 	
 	/*
 	메서드 오버라이딩의 조건
-	-부모클래스에서 선언될때와 비교해서 접근범위가 동일하거나
-	그보다 넓으면 가능하다. 
-	- 부모에서 default로 선언하면 자식에서는 default 혹은 
-	그보다 넓은 public으로 선언하면 오버라이딩이 가능하다. 
+	1.부모클래스에서 선언될때와 비교해서 접근범위가 동일하거나
+		그보다 넓으면 가능하다.
+	2.부모에서 default로 선언하면 자식에서는 default 혹은 그보다
+		넓은 public으로 선언하면 오버라이딩이 성립한다. 
 	 */
 	@Override
 	public String sleep() {
@@ -57,20 +60,19 @@ public class DeChild extends DeParent {
 		return 0;
 	}
 	
-	//오버라이딩 정의. 메서드의 형태가 완전히 동일하다. 
+	//오버라이딩 정의. 메서드의 형태가 완전히 동일함.
 	@Override
 	public void excecise() {
 		System.out.println("학생이 운동한다.");
 	}
 	
-	//오버라이딩 정의. 
+	//오버라이딩 정의 
 	@Override
 	public void printParent() {
 		/*
 		super의 두가지 활용법
-		1.super() : 부모클래스의 생성자를 호출한다. 
-		2.super.멤버 : 부모의 멤버변수 혹은 멤버메서드를 
-			호출한다. 
+		1.super() : 부모클래스의 생성자를 호출한다 
+		2.super.멤버 : 부모의 멤버변수 혹은 멤버메서드를 호출한다. 
 		*/
 		super.printParent();
 		System.out.printf(", 학번:%s\n", stNumber);
@@ -78,20 +80,10 @@ public class DeChild extends DeParent {
 	
 	/*
 	static(정적) 메서드는 오버라이딩의 대상이 될수없다. 클래스
-	 	외부(메서드영역)에 미리 로드되므로 오버라이딩 되지않고, 
-	 	호출시 각 클래스명을 통해 사용해야한다. 
+	외부(메서드영역)에 별도로 로드되므로 오버라이딩이 되지않고
+	호출시 각 클래스명을 통해 접근할 수 있다. 
 	*/
 	public static void staticMethod() {
 		System.out.println("학생의 정적메소드");
 	}
 }
-
-
-
-
-
-
-;
-
-
-
--- src/ex12inheritance/E08VariableArgument.java
+++ src/ex12inheritance/E08VariableArgument.java
@@ -1,8 +1,9 @@
 package ex12inheritance;
+
 /*
 가변인자(Variable Argument)
 : 메서드 오버로딩을 대체할 수 있는 문법으로 하나의 메서드로 여러가지
-호출형식을 대체할수있다. 
+호출형식을 대체할 수 있다. 
 형식]
 	메서드명(자료형 ... 매개변수명){
 		실행문장;
@@ -10,10 +11,11 @@ package ex12inheritance;
 	-필요에 따라 매개변수를 가변적으로 조정할 수 있는 문법
 	-매개변수로 2개이상의 인수를 받을 수 있어야 하므로 배열로 반환
 	-단, 자료형은 모두 동일해야 한다. 
-*/
+ */
 public class E08VariableArgument {
  
-	//앞에서 학습한 메서드 오버로딩을 통한 메서드 정의(여러개를 만들긴 어렵다) 
+	//앞에서 학습한 메서드 오버로딩을 통한 메서드 정의(여러개를 만들긴
+	//어렵다)
 //	static int getTotal(int param) {
 //		int total = 0;
 //		total += param;
@@ -31,13 +33,13 @@ public class E08VariableArgument {
 //	}
 	
 	/*
-	...을 통해 매개변수 param이 2개이상의 인수를 받을 수 있는것을
+	... 을 통해 매개변수 param이 2개 이상의 인수를 받을 수 있는것을 
 	명시한다. 또한 메서드 내에서는 배열로 반환되어 "매개변수명.length"와
-	같이 사용할 수 있다. 
-	*/
+	같이 코딩할 수 있다. 
+	 */
 	static int getTotal(int ... param) {
 		int total = 0;
-		//전달된 매개변수의 크기만큼 반복하여 누적해서 더한다. 
+		//전달된 인수의 크기만큼 반복하여 누적해서 더한다. 
 		for(int i=0 ; i<param.length ; i++) {
 			total += param[i];
 		}
@@ -46,8 +48,10 @@ public class E08VariableArgument {
 
 	public static void main(String[] args) {
 		
-		/* 가변인자를 통해 메서드를 정의하였으므로 아래 모든 호출문장은 
-		정상적으로 실행된다. */
+		/*
+		가변인자를 통해 메서드를 정의하였으므로 아래 모든 호출문장은 
+		정상적으로 실행된다. 
+		 */
 		System.out.println("getTotal(args1)호출:"+
 				getTotal(10));	
 		System.out.println("getTotal(args1,args2)호출:"+
@@ -57,16 +61,12 @@ public class E08VariableArgument {
 		System.out.println("getTotal(매개변수6개)호출:"+
 				getTotal(10,20,30,40,50,60));
 		
-		/* 매개변수의 타입이 double이 포함되어 있으므로 호출할 수 없다. 
-		만약 아래와 같이 실수를 모두 처리하려면 가변인자의 타입을 
-		double로 선언하면된다. */
-//		System.out.println("getTotal(args1, args2)호출:"+ 
-//				getTotal(10, 20.3));
-		 
+		/*
+		매개변수의 타입이 double이 포함되어 있으므로 호출할 수 없다. 
+		만약 아래와 같이 실수를 모두 처리하려면 가변인자의 타입을
+		double로 선언하면 된다. 
+		 */
+//		System.out.println("잘못된호출=", getTotal(10, 3.14));
 	}
 }
-
-
-
-
 
